064f6eeff9d578c1307f0707476929e422e92c0e
yassermokaddem32 <yassermokaddem95@gmail.com>
2026-04-27 10:44:56 +0100

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    formats: ["image/avif", "image/webp"]
+  }
+};
+
+export default nextConfig;
+
